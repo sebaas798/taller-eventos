fafeac9d2970580db7e8cc9dc8e3b7f706fbaedd
sebaas798 <ssoba93@gmail.com>
2023-08-11 19:31:50 -0300

    3
    aparece solo la alerta dentro del div

--- eventos.js
+++ eventos.js
@@ -1,8 +1,10 @@
 const divBoton = document.getElementById("contenedor");
-
+const boton = document.querySelector("#boton")
 
 function clicEnDiv() {
     alert("Hola! Soy el div");
 }
 
 divBoton.addEventListener("click", clicEnDiv);
+
+clicEnDiv.stopPropagation();
